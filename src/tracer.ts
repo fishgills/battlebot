748d6f7de683ba8b9612884704b15ae57d0e52d7
Charlie Davis <cdavis@hq.bill.com>
2021-12-13 11:34:14 -0800

    more

--- src/tracer.ts
+++ src/tracer.ts
@@ -2,14 +2,16 @@ import tracer from 'dd-trace';
 
 import { execSync } from 'child_process';
 
-const getGitVersion = () => execSync('git rev-parse HEAD').toString().trim();
+// const getGitVersion = () => execSync('git rev-parse HEAD').toString().trim();
 
 // initialized in a different file to avoid hoisting.
-tracer.init({
-  env: process.env['DD_ENV'],
-  service: 'GraphQL',
-  version: getGitVersion(),
-});
-tracer.use('graphql');
+if (process.env.NODE_ENV === 'production') {
+  tracer.init({
+    env: process.env['DD_ENV'],
+    service: 'GraphQL',
+    // version: getGitVersion(),
+  });
+  tracer.use('graphql');
+}
 
 export default tracer;
