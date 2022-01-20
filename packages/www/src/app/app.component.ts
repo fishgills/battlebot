6b77a84c07ee0941dbf35ffd7d6414b9f2601c23
Charlie Davis <fishgills@fishgills.net>
2022-01-20 00:43:03 -0800

    asdf

--- packages/www/src/app/app.component.ts
+++ packages/www/src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { PopupLoginService } from './popup-login.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private service: PopupLoginService) {}
+  ngOnInit(): void {
+    this.service.checkAuth();
+  }
+}
