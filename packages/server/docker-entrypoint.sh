41febf32f86d9e1b32fc525fa005d3052f3fc452
Charlie Davis <fishgills@fishgills.net>
2022-02-09 17:09:16 -0800

    asdf

--- packages/server/docker-entrypoint.sh
+++ packages/server/docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -eu
 echo "starting entrypoint" >&1
 cd /tmp
-yarn typeorm -f ./ormconfig-migrations.js migration:show
 yarn typeorm -f ./ormconfig-migrations.js migration:run
 node main.js
 echo "express started" >&1
