f08babba6d460ab0bedd82d4a62a093af72ee504
Charlie Davis <fishgills@fishgills.net>
2022-03-11 08:31:02 -0800

    blah

--- packages/bot/src/tracer.ts
+++ packages/bot/src/tracer.ts
@@ -1,22 +1,9 @@
 import tracer from 'dd-trace';
-import axios from 'axios';
-
-// const getGitVersion = () => execSync('git rev-parse HEAD').toString().trim();
-
-// initialized in a different file to avoid hoisting.
 if (process.env.NODE_ENV === 'production') {
-  const mytracer = tracer.init({
+  tracer.init({
     env: process.env['NODE_ENV'],
     service: 'bot',
   });
-
-  // axios
-  //   .get('http://169.254.169.254/latest/meta-data/local-ipv4')
-  //   .then((resp) => {
-  //     console.log('Host Data: ', JSON.stringify(resp));
-
-  //     mytracer.setUrl(`http://${resp.data}:8126`);
-  //   });
 }
 
 export default tracer;
