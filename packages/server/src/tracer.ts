3ab8a81b9ee77527dd8768f35ee5e367db93f1c7
Charlie Davis <fishgills@fishgills.net>
2022-03-10 18:38:34 -0800

    blah

--- packages/server/src/tracer.ts
+++ packages/server/src/tracer.ts
@@ -11,12 +11,12 @@ if (process.env.NODE_ENV === 'production') {
   });
   mytracer.use('graphql');
 
-  axios
-    .get('http://169.254.169.254/latest/meta-data/local-ipv4')
-    .then((resp) => {
-      console.log('Host Data: ', JSON.stringify(resp));
-      mytracer.setUrl(`http://${resp.data}:8126`);
-    });
+  // axios
+  //   .get('http://169.254.169.254/latest/meta-data/local-ipv4')
+  //   .then((resp) => {
+  //     console.log('Host Data: ', JSON.stringify(resp));
+  //     mytracer.setUrl(`http://${resp.data}:8126`);
+  //   });
 }
 
 export default tracer;
