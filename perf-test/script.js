a68c145f16710f20ea1ea4993a698f6b25b82b3a
Charlie Davis <fishgills@fishgills.net>
2022-03-14 01:04:15 -0700

    blah

---
+++ perf-test/script.js
@@ -0,0 +1,54 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+let accessToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImJvdCIsInN1YiI6IjBlMjZiODYzLTRlYzQtNGMyOC04MDhmLTVkOGVlMzYyYTc5ZCIsImlhdCI6MTY0NzI0MjIxMCwiZXhwIjoxNjQ3MjQ0MDEwfQ.9dc0u3dm759-tA5u1-eZpKVXCT6i120Lhx0pl-ot7pA';
+
+export default function () {
+  let query = `
+    query FindByOwner {
+  findByOwner(owner: "U01CJ3YFV6W", teamId: "T01CJA1PZHR") {
+    id
+    attacking {
+      id
+    }
+  }
+}`;
+
+  let headers = {
+    Authorization: `Bearer ${accessToken}`,
+    'Content-Type': 'application/json',
+  };
+
+  let res = http.post(
+    'https://api.slackbattlebot.com/graphql',
+    JSON.stringify({ query: query }),
+    { headers: headers },
+  );
+
+  if (res.status === 200) {
+    console.log(JSON.stringify(res.body));
+    let body = JSON.parse(res.body);
+    let issue = body.data.repository.issues.edges[0].node;
+    console.log(issue.id, issue.number, issue.title);
+
+    let mutation = `
+      mutation AddReactionToIssue {
+        addReaction(input:{subjectId:"${issue.id}",content:HOORAY}) {
+          reaction {
+            content
+          }
+          subject {
+            id
+          }
+        }
+    }`;
+
+    res = http.post(
+      'https://api.slackbattlebot.com/graphql',
+      JSON.stringify({ query: mutation }),
+      { headers: headers },
+    );
+  }
+  sleep(0.3);
+}
