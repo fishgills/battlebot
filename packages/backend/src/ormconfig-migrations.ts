07f58aa2c3e851df1d3f192fe59db30fe6f85484
Charlie Davis <fishgills@fishgills.net>
2022-03-16 13:26:04 -0700

    blah

--- packages/backend/src/ormconfig-migrations.ts
+++ packages/backend/src/ormconfig-migrations.ts
@@ -1,3 +1,32 @@
 import { database } from './config/database.config';
+import * as AWS from 'aws-sdk';
 
-export = database;
+const getConn = async () => {
+  if (process.env.NODE_ENV === 'production') {
+    const ssm = new AWS.SecretsManager({
+      region: 'us-east-1',
+    });
+
+    const result = await ssm
+      .getSecretValue({
+        SecretId:
+          'arn:aws:secretsmanager:us-east-1:946679114937:secret:rds-info-1-MwazwX',
+      })
+      .promise();
+
+    const { host, username, password } = JSON.parse(result.SecretString);
+    const config = {
+      ...database,
+      host,
+      username,
+      password,
+    };
+    console.log(config);
+    return config;
+  } else {
+    console.log(database);
+    return database;
+  }
+};
+
+export = getConn();
