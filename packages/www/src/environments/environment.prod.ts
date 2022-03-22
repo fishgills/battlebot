c3d3f3bd1a5ad7e70ff9405a779a7c5d27d4a360
Charlie Davis <fishgills@fishgills.net>
2022-03-22 11:48:43 -0700

    blah

--- packages/www/src/environments/environment.prod.ts
+++ packages/www/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
-  hostname: 'slackbattlebot.com',
-  gql: 'https://api.slackbattlebot.com/graphql',
+  hostname: 'mediatingmediator.com',
+  gql: 'https://backend.mediatingmediator.com/graphql',
   stripe: {
     enabled: true,
     stripe_key:
       'pk_live_51KVVrJIhYgDhNhELgzFu6dnAwoo5pJl0gko4SRs0ZLPSt1yQrCJGpeieJXYW9OZMoI0A1kjCV7PJtq1iVjHUbMT600kLi1y0Kz',
-    successUrl: 'https://api.slackbattlebot.com/stripe/success',
-    cancelUrl: 'https://api.slackbattlebot.com/stripe/cancel',
+    successUrl: 'https://backend.mediatingmediator.com/stripe/success',
+    cancelUrl: 'https://backend.mediatingmediator.com/stripe/cancel',
     productKey: 'price_1KW9axIhYgDhNhELD1GW3qv6',
   },
 };
