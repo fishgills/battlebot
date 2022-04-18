14da4a9d8064b16249b3b9ffe0c25e34743fd501
Charlie Davis <fishgills@fishgills.net>
2022-04-18 00:58:37 -0700

    blah

--- packages/www/src/app/app-routing.module.ts
+++ packages/www/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrivacyComponent } from './privacy/privacy.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,10 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'privacy',
+    component: PrivacyComponent,
   },
   {
     path: 'tavern',
