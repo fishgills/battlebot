0d1c59c9d5e3108cb7f0dd3d54b11fd311c25100
Charlie Davis <fishgills@fishgills.net>
2022-07-21 02:49:12 -0700

    progress

--- packages/www/src/app/how/how.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-how',
-  templateUrl: './how.component.html',
-  styleUrls: ['./how.component.css'],
-})
-export class HowComponent {}
