a480ed267c9e92cf4ec4bbea993ef36658a8596f
Charlie Davis <fishgills@fishgills.net>
2023-02-03 01:36:17 -0800

    progress

--- packages/backend/docker-entrypoint.sh
+++ packages/backend/docker-entrypoint.sh
@@ -1,6 +1,5 @@
-#!/bin/sh -eu
+#!/bin/sh
 echo "starting entrypoint" >&1
 cd /app/packages/$APP/
 yarn typeorm -d ./ormconfig-migrations.js migration:run
 node main.js
-echo "backend started" >&1
--- packages/bot/docker-entrypoint.sh
+++ packages/bot/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "Starting the slack bot"
+echo "Starting the slack bot!"
 node index.js
