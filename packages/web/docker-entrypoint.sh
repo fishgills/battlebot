130053bc53f22a15390a49b62758dcf109a83bb8
Charlie Davis <fishgills@fishgills.net>
2023-02-07 23:20:38 -0800

    progress

--- packages/web/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -eu
-mkdir /next
-cd /next
-cp /app/packages/web/public /next/public
-cp /app/packages/web/dist/standalone/* /next/
-cp /app/packages/web/dist/static/* /next/static
-cp /app/packages/web/node_modules/* /next/node_modules/
