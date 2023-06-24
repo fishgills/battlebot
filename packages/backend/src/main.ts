8946ae260c008644927bc1b3528b0d011a02f359
Charlie <fishgills@fishgills.net>
2023-06-24 14:21:17 -0700

    ADDED: dotenv dependency and updated version
    - Added "dotenv": "^16.3.1" to the dependencies in package.json.
    - Imported 'dotenv/config' in main.ts file.
    - Updated app.listen() to use process.env.PORT or default to 3000 in main.ts.
    - Updated yarn.lock file to include dotenv@16.3.1 and typescript@^5.1.3.

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
@@ -9,6 +10,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useGlobalFilters(new OrmExceptionFilter());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
